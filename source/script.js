beb782cd68dbbb1f51d68ff928407be4674d9c08
Clemens Damke <admin@cortys.de>
2014-05-02 20:08:40 +0200

    Updated source

--- source/script.js
+++ source/script.js
@@ -13,7 +13,13 @@ function copy(str, mimetype) {
 	document.execCommand("Copy", false, null);
 }
 
-function tryShorten() {
+function tryShorten(pURL) {
+	if(!navigator.onLine) {
+		alert("You seem to be offline.");
+		return;
+	}
+	if(!pURL)
+		pURL = currentURL;
 	changeIcon("Y");
 	sGet(["token"], function(data) {
 		if(!data.token) {
@@ -21,10 +27,19 @@ function tryShorten() {
 			showDialog();
 			return;
 		}
-		$.get("https://api-ssl.bitly.com/v3/shorten", { "access_token":data.token, longUrl:encodeURI(currentURL) }, function(data) {
+		$.get("https://api-ssl.bitly.com/v3/shorten", { "access_token":data.token, longUrl:encodeURI(pURL) }, function(data) {
 			if(data["status_txt"] == "OK") {
 				copy(data.data.url, "text/plain");
 				changeIcon("G");
+				setTimeout(function() {
+					changeIcon("");
+					setTimeout(function() {
+						changeIcon("G");
+						setTimeout(function() {
+							changeIcon("");
+						}, 200);
+					}, 100);
+				}, 200);
 			}
 			else {
 				changeIcon("R", function() {
@@ -33,10 +48,10 @@ function tryShorten() {
 					else
 						alert("Your request failed! Please try again.");
 				});
+				setTimeout(function() {
+					changeIcon("");
+				}, 300);
 			}
-			setTimeout(function() {
-				changeIcon("");
-			}, 300);
 		}, "json").fail(function() {
 			refreshToken(tryShorten, showDialog);
 		});
@@ -47,3 +62,26 @@ function tryShorten() {
 function showDialog() {
 	chrome.tabs.create({ url:"options.html?fail" }, tabClosing);
 }
+
+// CONTEXT MENU:
+chrome.contextMenus.removeAll(function() {
+	chrome.contextMenus.create({
+		type:"normal",
+		id:"bitTs",
+		title:"that shit",
+		contexts:["page","link","image","audio","video"]
+	});
+	chrome.contextMenus.onClicked.addListener(function(info) {
+		if(info.menuItemId == "bitTs") {
+			var url;
+			if(info.linkUrl)
+				url = info.linkUrl;
+			else if(info.mediaType)
+				url = info.srcUrl;
+			else
+				url = info.frameUrl?info.frameUrl:info.pageUrl;
+			console.log(info);
+			tryShorten(url);
+		}
+	});
+});
